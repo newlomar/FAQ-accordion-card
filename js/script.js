521209158886db04c0109d82cdca5eec25aa156e
Newton Lomar <newtonlomar16@gmail.com>
2021-08-14 18:56:55 -0300

    first js idea development

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
 const questions_div = document.querySelector('.questions');
 
-console.log(questions_div);
+const questions = [...questions_div.children];
+
+questions[0].children[0].children[0].className = 'active';
+
+questions[0].children[1].classList += ' active';
