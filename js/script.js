0459ac280d67bda24f2f2b791fe570b93fed1672
Newton Lomar <newtonlomar16@gmail.com>
2021-08-15 23:31:28 -0300

    keep going with javascript logic

--- js/script.js
+++ js/script.js
@@ -2,6 +2,11 @@ const questions_div = document.querySelector('.questions');
 
 const questions = [...questions_div.children];
 
-questions[0].children[0].children[0].className = 'active';
+function add_active_class() {
+  questions.forEach(item => {
+    item.children[0].children[0].classList += ' active';
+    item.children[1].classList += ' active';
+  });
+}
 
-questions[0].children[1].classList += ' active';
+add_active_class();
